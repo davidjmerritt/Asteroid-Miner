54367673bb739a5c3aedb54fe7f726567d4874ae
Merritt, David <David.Merritt@comedycentral.com>
2016-12-22 13:36:43 -0500

    Fixed iOS detect bug

--- js/sketch.js
+++ js/sketch.js
@@ -53,9 +53,6 @@ function setup() {
         reset();
       }
     });
-  } else {
-    menu = new Menu();
-    menu.unSupportedDevice();
   }
   reset();
 }
@@ -113,140 +110,146 @@ function draw() {
   background(0);
   menu.render();
 
-  if (paused) { menu.gameScore('Paused'); } else {
-    if (gameStarted == false) {
-      menu.gameStart();
-    } else {
-      if (ship.isAlive) {
-        //menu.gamePlay();
-        for (var i = 0; i < asteroids.length; i++) {
-          if (ship.hits(asteroids[i])) {
-            background(255,0,0);
-            ship.explode();
+  if (supportedMobileDevice == true || supportedBrowser == true) {
+    
+    if (paused) { menu.gameScore('Paused'); } else {
+      if (gameStarted == false) {
+        menu.gameStart();
+      } else {
+        if (ship.isAlive) {
+          //menu.gamePlay();
+          for (var i = 0; i < asteroids.length; i++) {
+            if (ship.hits(asteroids[i])) {
+              background(255,0,0);
+              ship.explode();
+            }
+            asteroids[i].render();
+            asteroids[i].update();
+            asteroids[i].edges();
           }
-          asteroids[i].render();
-          asteroids[i].update();
-          asteroids[i].edges();
-        }
 
-        for (var i = 0; i < energyUp.length; i++) {
-          if (ship.hits(energyUp[i])) {
-            energyUp.splice(i, 1);
-            ship.enhance();
-          }
-          if (energyUp.length > 0) {
-            energyUp[i].render();
-            energyUp[i].update();
-            energyUp[i].edges();
-          }
-        }
-        for (var i = 0; i < energyDown.length; i++) {
-          if (ship.hits(energyDown[i])) {
-            energyDown.splice(i, 1);
-            ship.weaken();
-          }
-          if (energyDown.length > 0) {
-            energyDown[i].render();
-            energyDown[i].update();
-            energyDown[i].edges();
-          }
-        }
-
-        for (var i = 0; i < rangeUp.length; i++) {
-          if (ship.hits(rangeUp[i])) {
-            rangeUp.splice(i, 1);
-            laserRange += 10;
-          }
-          if (rangeUp.length > 0) {
-            rangeUp[i].render();
-            rangeUp[i].update();
-            rangeUp[i].edges();
-          }
-        }
-
-        for (var i = 0; i < rangeDown.length; i++) {
-          if (ship.hits(rangeDown[i])) {
-            rangeDown.splice(i, 1);
-            laserRange -= 10;
-            if (laserRange < 20) {
-              laserRange = 10;
+          for (var i = 0; i < energyUp.length; i++) {
+            if (ship.hits(energyUp[i])) {
+              energyUp.splice(i, 1);
+              ship.enhance();
+            }
+            if (energyUp.length > 0) {
+              energyUp[i].render();
+              energyUp[i].update();
+              energyUp[i].edges();
             }
           }
-          if (rangeDown.length > 0) {
-            rangeDown[i].render();
-            rangeDown[i].update();
-            rangeDown[i].edges();
+          for (var i = 0; i < energyDown.length; i++) {
+            if (ship.hits(energyDown[i])) {
+              energyDown.splice(i, 1);
+              ship.weaken();
+            }
+            if (energyDown.length > 0) {
+              energyDown[i].render();
+              energyDown[i].update();
+              energyDown[i].edges();
+            }
           }
-        }
 
-        for (var i = lasers.length - 1; i >= 0; i--) {
-          lasers[i].render();
-          lasers[i].update();
-          if (lasers[i].dist > laserRange) {
-            lasers.splice(i, 1);
-          } else if (lasers[i].offscreen()) {
-            lasers.splice(i, 1);
-          } else {
-            for (var j = asteroids.length - 1; j >= 0; j--) {
-              if (lasers[i].hits(asteroids[j])) {
-                if (asteroids[j].r > 10) {
-                  var newAsteroids = asteroids[j].breakup();
-                  asteroids = asteroids.concat(newAsteroids);
+          for (var i = 0; i < rangeUp.length; i++) {
+            if (ship.hits(rangeUp[i])) {
+              rangeUp.splice(i, 1);
+              laserRange += 10;
+            }
+            if (rangeUp.length > 0) {
+              rangeUp[i].render();
+              rangeUp[i].update();
+              rangeUp[i].edges();
+            }
+          }
+
+          for (var i = 0; i < rangeDown.length; i++) {
+            if (ship.hits(rangeDown[i])) {
+              rangeDown.splice(i, 1);
+              laserRange -= 10;
+              if (laserRange < 20) {
+                laserRange = 10;
+              }
+            }
+            if (rangeDown.length > 0) {
+              rangeDown[i].render();
+              rangeDown[i].update();
+              rangeDown[i].edges();
+            }
+          }
+
+          for (var i = lasers.length - 1; i >= 0; i--) {
+            lasers[i].render();
+            lasers[i].update();
+            if (lasers[i].dist > laserRange) {
+              lasers.splice(i, 1);
+            } else if (lasers[i].offscreen()) {
+              lasers.splice(i, 1);
+            } else {
+              for (var j = asteroids.length - 1; j >= 0; j--) {
+                if (lasers[i].hits(asteroids[j])) {
+                  if (asteroids[j].r > 10) {
+                    var newAsteroids = asteroids[j].breakup();
+                    asteroids = asteroids.concat(newAsteroids);
+                  }
+                  asteroids.splice(j, 1);
+                  lasers.splice(i, 1);
+                  score += 1;
+                  break;
                 }
-                asteroids.splice(j, 1);
-                lasers.splice(i, 1);
-                score += 1;
-                break;
               }
             }
           }
-        }
 
-        for (var i = 0; i < powers.length; i++) {
-          powers[i].render();
-          powers[i].update();
-          powers[i].edges();
-          if (ship.hits(powers[i])) {
-            powers.splice(i, 1);
-            laserSize += 20;
+          for (var i = 0; i < powers.length; i++) {
+            powers[i].render();
+            powers[i].update();
+            powers[i].edges();
+            if (ship.hits(powers[i])) {
+              powers.splice(i, 1);
+              laserSize += 20;
+            }
           }
-        }
 
-        ship.render();
-        ship.turn();
-        ship.update();
-        ship.edges();
+          ship.render();
+          ship.turn();
+          ship.update();
+          ship.edges();
 
-        menu.gameScore('Level '+numberOfAsteroids+' | Score '+score);
+          menu.gameScore('Level '+numberOfAsteroids+' | Score '+score);
 
-        if (asteroids.length <= 0) {
-          // menu.gameWin();
-          numberOfAsteroids += 1;
-          restarted = false;
-          reset();
+          if (asteroids.length <= 0) {
+            // menu.gameWin();
+            numberOfAsteroids += 1;
+            restarted = false;
+            reset();
+            // var t = setTimeout(function(){
+            //   clearTimeout(t);
+            // },3000);
+
+
+            ship.isAlive = true;
+
+          }
+
+        } else {
+          menu.gameOver('Level '+numberOfAsteroids+' | Score '+score);
+          gameStarted = false;
           // var t = setTimeout(function(){
+          //   // gameStarted = false;
+          //   numberOfAsteroids = 0;
+          //   gameStarted = false;
+          //   ship.isAlive = false;
+          //   reset();
           //   clearTimeout(t);
           // },3000);
-
-
-          ship.isAlive = true;
-
         }
-
-      } else {
-        menu.gameOver('Level '+numberOfAsteroids+' | Score '+score);
-        gameStarted = false;
-        // var t = setTimeout(function(){
-        //   // gameStarted = false;
-        //   numberOfAsteroids = 0;
-        //   gameStarted = false;
-        //   ship.isAlive = false;
-        //   reset();
-        //   clearTimeout(t);
-        // },3000);
       }
     }
-  }
+  } else {
+      menu = new Menu();
+      menu.unSupportedDevice();
+    }
 }
 
 function mouseReleased() {
