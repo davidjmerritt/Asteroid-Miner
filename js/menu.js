48ee1fd0002a47d52d75d713660252439bf521b3
Merritt, David <David.Merritt@comedycentral.com>
2016-12-24 07:55:08 -0500

    Updated instructions.

--- js/menu.js
+++ js/menu.js
@@ -7,10 +7,7 @@ function Menu() {
     this.message = "You are currently using an unsupported device.  Try on an iPad Mini or Desktop.";
   }
   this.gameStart = function() {
-    this.message = "ASTEROID MINER\n\n"
-    this.message += "BREAK ASTEROID = 1 POINT\nCOLLECT COAL = 5 POINTS\nCOLLECT IRON = 10 POINTS\nCOLLECT GOLD = 20 POINTS\n\n"
-    this.message += "SPACEBAR = FIRE/START\nARROW-UP,W = BOOST\nARROW-DOWN,V = PAUSE\nARROW-LEFT = ROTATE LEFT\nARROW-RIGHT = ROTATE RIGHT\n\n";
-
+    this.message = "ASTEROID MINER\n\nSPACEBAR = FIRE/START\nARROW-UP,W = BOOST\nARROW-DOWN,V = PAUSE\nARROW-LEFT = ROTATE LEFT\nARROW-RIGHT = ROTATE RIGHT\n\nBREAK ASTEROID = 1 POINT\nCOLLECT COAL = 5 POINTS\nCOLLECT IRON = 10 POINTS\nCOLLECT GOLD = 20 POINTS\nCOLLECT E/e = INCREASE/DECREASE SHIELDS\nCOLLECT T/t = CHANGE/REMOVE RANDOM LASER TYPE (NORMAL, BACK-FIRE, SPREAD)\nCOLLECT R/r = TEMPORARILY INCREASE/DECREASE LASER RANGE\nCOLLECT P/p = INCREASE/DECREASE LASER SIZE";
   }
 
   this.gamePlay = function() {
@@ -32,10 +29,10 @@ function Menu() {
   this.render = function() {
     push();
     fill(this.col);
-    var size = 24
-    var x2 = 600;
+    var size = 22
+    var x2 = 800;
     var x1 = (width/2) - (x2 / 2);
-    var y2 = size*20;
+    var y2 = 500;
     var y1 = (height/2) - (y2 / 2);
     textAlign(CENTER);
     textFont(this.fontFace,size);
